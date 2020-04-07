4197d8e8d313d1e5011cdf08de6bc9cbbc967d50
ilgizDashkin <79374811484i@gmail.com>
2020-04-07 13:25:17 +0500

    add comments

--- backend/myrss.php
+++ backend/myrss.php
@@ -1,28 +1,30 @@
-<?php header('Access-Control-Allow-Origin: *'); //разрешаем кроссдоменные запросы CORS  ?>
+<?php header('Access-Control-Allow-Origin: *'); //разрешаем кроссдоменные запросы CORS  
+?>
 <?php
-// $url = "rss.xml";
+// $url = "rss.xml";//для тестов можно файл грузить
 $url = "https://www.rospotrebnadzor.ru/region/rss/rss.php?rss=y";
 
-function getFeeds($url) {
+function getFeeds($url)
+{
 
   $content = file_get_contents($url);
   $items = new SimpleXmlElement($content);
   $json = array();
   $json['rospotreb'] = [];
-//   echo "<ul>";
+  //   echo "<ul>";
 
-//   foreach($items -> channel -> item as $item) {
-//     echo "<li><a href = '{$item->link}' title = '$item->title'>" .
-//     $item->title . "</a> - " . $item -> description . "</li>";
-//   }
+  //   foreach($items -> channel -> item as $item) {
+  //     echo "<li><a href = '{$item->link}' title = '$item->title'>" .
+  //     $item->title . "</a> - " . $item -> description . "</li>";
+  //   }
 
-//   echo "</ul>";
-  
-  foreach($items -> channel -> item as $item) {
-     $json['rospotreb'][]= [[$item->title],[$item -> description]];
-    }
+  //   echo "</ul>";
+
+  foreach ($items->channel->item as $item) {
+    $json['rospotreb'][] = [[$item->title], [$item->description]];
+  }
   echo json_encode($json);
 }
-  
+
 getFeeds($url)
 ?>
